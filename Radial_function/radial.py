13a53a21c0c9c310687dd2446c348c7f26c51b0f
Zevsoverlord <mr_a953@mail.ru>
2020-06-26 16:41:48 +0300

    Update Radial function

--- Radial_function/radial.py
+++ Radial_function/radial.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 from math import *
+import numpy as np
 
 E_0 = 27.07 * 1.6 / (10 ** 19)
-a = 5.29 / (10 ** 11)
+a = 0.529
 R_H = 10957758.3407
 R_y = R_H * 3 * (10 ** 8) * 6.626 / (10 ** 34)
 n = [1, 2, 3, 4]
 dp = 0.01
 fig, ax = plt.subplots(figsize=(5, 5))
 E = [0] * 4
-F = 1
 
 
 def findEi(i):
@@ -24,32 +24,50 @@ def findBetta(epsilon):
     return sqrt(2 * epsilon)
 
 
-for i in range(len(n)):
-    R, p = [], []
-    pi = 0
-    p0 = 0
+R = []
+r = []
 
-    E[i] = findEi(i)
-    epsilon = findEpsilon(E[i])
-    betta = findBetta(epsilon)
+for j in range(1000):
+    r.append(j*dp)
 
-    z = n[i] * betta
-    l = i + 1
-    v = 0
-    m = 0
-    for k in range(1000):
-        F += factorial(v) * (2 * betta * p0) ** (k + 1) / factorial(2 * m + 2) / factorial(k)
-        v += 1
-        m += 1
-        p0 += dp
-        for j in range(1300):
-            R.append(
-                1 / (factorial(2 * i + 1)) * sqrt((factorial(i + 1) / factorial(0) * 2 * (i + 1))) * (2 * betta) ** (
-                        3 / 2) * exp(-betta * pi) * (2 * betta * pi) ** l * F)
-            p.append(pi)
-            pi += dp
-        if v == 4:
-            break
-    ax.plot(p, R)
+# N=1 L=0
+for i in r:
+    R.append(2/(a ** (3/2)) * exp(-i/a))
+ax.plot(r, R, label = "N=1 L=0")
+
+R = []
+
+# N=2 L=0
+for i in r:
+    R.append((1/sqrt(2*a ** 3) * (1-(i/(2*a))) * exp(-i/(2*a))))
+ax.plot(r, R, label = "N=2 L=0")
+
+R = []
+# N=2 L=1
+for i in r:
+    R.append((1/sqrt(6*a ** 3) * (i/a) * exp(-i/(2*a))))
+ax.plot(r, R, label = "N=2 L=1")
+
+R = []
+# N=3 L=0
+for i in r:
+    R.append((2/(3*sqrt(3*a ** 3)) * (1-(2*i/(3*a)) + (2*(i**2)/(27*(a**2)))) * exp(-i/(3*a))))
+ax.plot(r, R, label = "N=3 L=0")
+
+R = []
+# N=3 L=1
+for i in r:
+    R.append((8/(27*sqrt(6*a ** 3)) * (i/a) * (1-(i/(6*a))) * exp(-i/(3*a))))
+ax.plot(r, R, label = "N=3 L=1")
+
+R = []
+# N=3 L=2
+for i in r:
+    R.append((4/(81 * sqrt(30*a ** 3)) * ((i**2)/(a**2)) * exp(-i/(3*a))))
+ax.plot(r, R, label = "N=3 L=2")
+
+ax.legend()
+
+
 plt.grid()
 plt.show()
